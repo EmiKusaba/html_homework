7ff9537d8c52ef8988dbab5d764b12129086a0ab
EmiKusaba <eeemi615@gmail.com>
2022-11-30 13:44:29 -0600

    added game conditions

--- week3/script.js
+++ week3/script.js
@@ -1,6 +1,45 @@
-function PlayRound(playerSelection, computerSelection){
-  
+const selections = ["rock", "paper", "scissors"]
+let playerScore = 0;
+let computerScore = 0;
+
+
+function computerPlay(selections) {
+  return [Math.floor((Math.random()*selections.length))]
 }
-const playerSelection = "rock"
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection,computerSelection));
+
+
+function PlayRound(playerSelection, computerSelection) {
+  for (i = 0; i < 5; i++) {
+    
+  }
+}
+
+let playerSelection = []
+let computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
+function Game() {
+  playerSelection = playerSelection.trim().toLowerCase()
+  computerSelection = computerSelection.trim().toLowerCase()
+
+  if (computerSelection === playerSelection) {
+    return "It's a tie!";
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    return 'computer wins!';
+  }
+  else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+    return 'player wins!';
+  }
+  else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    return 'computer wins!';
+  }
+  else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    return 'computer wins!';
+  }
+  else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    return 'player wins!';
+  }
+  else if (playerSelection === 'scissors' && playerSelection === 'rock') {
+    return 'computer wins!'
+  }
+}
