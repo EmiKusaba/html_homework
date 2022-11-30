b63ca1d4f1e3b13dc7365122942ae73ec2301501
EmiKusaba <eeemi615@gmail.com>
2022-11-30 15:58:22 -0600

    added computerPlay func

--- week3/script.js
+++ week3/script.js
@@ -1,45 +1,50 @@
-const selections = ["rock", "paper", "scissors"]
-let playerScore = 0;
-let computerScore = 0;
+'use strict';
 
+let choice = prompt("Player: Rock, Paper or Scissors?");
 
-function computerPlay(selections) {
-  return [Math.floor((Math.random()*selections.length))]
+function computerPlay() {
+  let selections = ["rock", "paper", "scissors"]
+  let random = selections[Math.floor(Math.random() * selections.length)];
+  console.log("computer" + " " + random)
 }
 
+// function PlayRound(playerSelection, computerSelection) {
+//   let playerScore = 0;
+//   let computerScore = 0;
+//   for (i = 0; i < 5; i++) {
 
-function PlayRound(playerSelection, computerSelection) {
-  for (i = 0; i < 5; i++) {
-    
-  }
-}
+//   }
+// }
 
-let playerSelection = []
+let playerSelection = "";
 let computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
 
-function Game() {
-  playerSelection = playerSelection.trim().toLowerCase()
-  computerSelection = computerSelection.trim().toLowerCase()
 
-  if (computerSelection === playerSelection) {
-    return "It's a tie!";
-  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    return 'computer wins!';
+function Game(playerSelection, computerSelection) {
+  choice = playerSelection.trim().toLowerCase()
+  computerPlay()
+
+    if (computerSelection === playerSelection) {
+      console.log("It's a tie!");
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+      console.log('computer wins!');
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+      console.log('player wins!');
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
+      console.log('computer wins!');
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+      console.log ('computer wins!');
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+      console.log('player wins!');
+    }
+    else if (playerSelection === 'scissors' && playerSelection === 'rock') {
+      console.log('computer wins!');
+    }
   }
-  else if(playerSelection === 'rock' && computerSelection === 'scissors') {
-    return 'player wins!';
-  }
-  else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    return 'computer wins!';
-  }
-  else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    return 'computer wins!';
-  }
-  else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    return 'player wins!';
-  }
-  else if (playerSelection === 'scissors' && playerSelection === 'rock') {
-    return 'computer wins!'
-  }
-}
+
+  
