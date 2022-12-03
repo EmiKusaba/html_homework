e3586d5a9bfe90fe7959c92f102160e097f246fd
EmiKusaba <eeemi615@gmail.com>
2022-12-03 15:17:18 -0600

    added triangle

--- week3/Mathematical Shapes/script.js
+++ week3/Mathematical Shapes/script.js
@@ -2,5 +2,14 @@ function squere(s){
   return Math.sqrt(2) * s;
 }
 
+function triangle(side1,side2,side3){
+  var s = (side1 + side2 + side3)/2;
+  return Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+}
+
+//result
+//sq
 let s = 9
 console.log(squere(9))
+//traiangle
+console.log(triangle(5,6,7))
