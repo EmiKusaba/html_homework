9e5c0f76a96e6fef6b4d2a02bbb3b280af701093
EmiKusaba <eeemi615@gmail.com>
2022-12-04 13:58:04 -0600

    added all of mathmatical shapes

--- week3/Mathematical Shapes/script.js
+++ week3/Mathematical Shapes/script.js
@@ -7,9 +7,23 @@ function triangle(side1,side2,side3){
   return Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
 }
 
+function circumferenceOfCircle (circleRadius) {
+  return 2 * Math.PI * circleRadius;
+}
+function calculateArea(myRadius) {
+  return myRadius * myRadius * Math.PI;
+}
+
 //result
 //sq
 let s = 9
 console.log(squere(9))
 //traiangle
 console.log(triangle(5,6,7))
+//circumference and surface area
+let circleRadius = 4
+console.log(circumferenceOfCircle (circleRadius))
+
+let myRadius = 4
+console.log(calculateArea (myRadius))
+
