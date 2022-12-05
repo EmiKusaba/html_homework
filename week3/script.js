b08095c20ce2a4d570eec41d7f0c92ef347bbb96
EmiKusaba <eeemi615@gmail.com>
2022-12-05 11:18:46 -0600

    fixed error

--- week3/script.js
+++ week3/script.js
@@ -2,7 +2,7 @@ let playerScore = 0;
 let computerScore = 0;
 
 function getPlayerSelection() {
-  let userInput = prompt("Player: Rock, Paper or Scissors?").trim().toLowerCase();
+  let userInput = prompt("Player: Rock, Paper or Scissors?").toLowerCase();
   if (userInput === "paper" || userInput === "rock" || userInput === "scissors") {
     console.log("You chose " + `${userInput}.`)
   } else {
